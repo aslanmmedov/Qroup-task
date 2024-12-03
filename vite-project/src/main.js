9be1f8e587117b5624c1123dcf24984c1031e28a
asgrlifarid <faridaskerli2@gmail.com>
2024-12-04 01:12:34 +0400

    some changes at branch of Sabina

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,6 @@
+const menuToggle = document.querySelector(".new-menu-item");
+const header = document.getElementById("header");
 
+menuToggle.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
